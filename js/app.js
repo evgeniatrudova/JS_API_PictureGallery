8ee108fdb633cde8aeec44d8fea8eb2502119c89
Evgenia Trudova <68112616+evgeniatrudova@users.noreply.github.com>
2021-11-30 21:18:35 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const API_KEY ='1aa73561a242a3323423e405256871bf';
-const API_URL_DINOSAURS = 'https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=21f2f2772a3f8e11f5d18959a8d63eca&tags=dinosaurs&sort=period&per_page=20&format=json&nojsoncallback=1&api_sig=5080245e02959dc1e7eb744fe404bd22';
+const API_URL_DINOSAURS = 'https://dog.ceo/api/breeds/image/random';
 
 getPictures (API_URL_DINOSAURS);
 async function getPictures(url){
